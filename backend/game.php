f9de5f5c629cb75e8c40881e67e54be85ded87ec
prodev08 <omnideveloper.soft@gmail.com>
2024-01-30 09:21:51 +0000

    Added getState and restart game function

--- backend/game.php
+++ backend/game.php
@@ -1,5 +1,6 @@
 <?php
 session_start();
+include './common.php';
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $game_id = $_SESSION['game_id'];
@@ -9,9 +10,25 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     if ($action == 'move') {
         $cell_index = $_POST['cell_index'];
     } elseif ($action == 'state') {
-        echo 'game state';
+        $state = getGameState($game_id);
+        echo json_encode($state);
     } elseif ($action == 'restart') {
         // Handle the restart action
+        if ($game_id && isset($_SESSION['game_state'][$game_id])) {
+            restartGame($game_id);
+        }
     }
 }
+
+function restartGame($game_id)
+{
+    $players = $_SESSION['game_state'][$game_id]['players'];
+    $_SESSION['game_state'][$game_id] = [
+        'board' => array_fill(0, 9, null),
+        'players' => $players,
+        'current_player' => 'X',
+        'winner' => null,
+    ];
+}
+
 ?>
